78b22e2e3a4a5af25eb1157340edb7448d96f4c7
skuetoDev <mariogn1987@gmail.com>
2025-11-02 16:32:18 +0100

    some bugs in p of post and h2 footer

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+// ** Database settings - You can get this info from your web host ** //
+
+/** The name of the database for WordPress */
 define( 'DB_NAME', 'u807378594_pruebawordpres' );
 
 /** Database username */
