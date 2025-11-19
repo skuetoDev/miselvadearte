0cd8a1ccd3aee601fd9f02ffb41a8066867c8330
skuetoDev <mariogn1987@gmail.com>
2025-11-19 09:55:19 +0100

    favicon bug

--- wp-content/themes/temaPrueba/header-page.php
+++ wp-content/themes/temaPrueba/header-page.php
@@ -10,7 +10,7 @@
         <meta charset="<?php bloginfo('charset'); ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
         <!-- para importar estilos desde functions.php -->
-       
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon"/>
         <?php wp_head(); ?>
       </head>
       <div id="page-wrapper">
