605e1bfdb90fdef13b6240f3809cb6656007268f
skuetoDev <mariogn1987@gmail.com>
2026-02-06 19:19:50 +0100

    footer background and short category list

--- wp-content/themes/temaPrueba/footer.php
+++ wp-content/themes/temaPrueba/footer.php
@@ -1,5 +1,8 @@
 
 <section id="footer">
+   <div class="background-image">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/footer-background.svg" alt="footer background">
+   </div>
    <div class="container">
       <div class="row">
          <div class="col-4 col-12-medium">
@@ -14,9 +17,14 @@
          </div>
          <div class="col-4 col-6-medium col-12-small">
             <section>
-               <ul class="divided">
+               <ul class="divided category">
                   <?php
-                        dynamic_sidebar('categorias');
+                        wp_list_categories(array(
+                           'number'     => 5,
+                           'title_li'   => '<h2 class="wp-block-heading" >Categorías</h2>',
+                           'orderby'    => 'count',
+                           'order'      => 'DESC',
+                        ));
                     ?>
                </ul>
             </section>
