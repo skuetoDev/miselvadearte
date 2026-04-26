2e7a4fce74726c53aeed05ced934c4dbc4bc09b3
skuetoDev <mariogn1987@gmail.com>
2026-04-26 17:18:25 +0200

    img 404

--- wp-content/themes/temaPrueba/404.php
+++ wp-content/themes/temaPrueba/404.php
@@ -5,7 +5,7 @@
          <div class="container">
 
             <!-- Content -->
-            <article class="box post">
+            <article class="box post-404">
                <div>
                   <h2 class="title-404">404 - Página no encontrada</h2>
                   <img class="img-404" src="<?php echo get_template_directory_uri(); ?>/images/404.webp" alt="404 - Página no encontrada - Scraplion"/>
