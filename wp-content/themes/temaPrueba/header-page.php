37ad833e43d42c516d2c7ac237a1f7ef1ac1d1b4
skuetoDev <mariogn1987@gmail.com>
2026-04-26 18:07:17 +0200

    links movil

--- wp-content/themes/temaPrueba/header-page.php
+++ wp-content/themes/temaPrueba/header-page.php
@@ -3,7 +3,7 @@
       <head>
         <title><?php echo bloginfo('name'); ?></title>
         <meta charset="<?php bloginfo('charset'); ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <!-- para importar estilos desde functions.php -->
         <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon"/>
         <?php wp_head(); ?>
